f5d856cc8f46fc723fc3e1bb2e9293a7fb2253ce
Carolina Berrafato <85506019+carolinaberrafato@users.noreply.github.com>
2023-04-25 21:22:09 -0300

    Adicionei o atributo "vegetariano" nas consultas que faltavam

--- consultas.js
+++ consultas.js
@@ -63,7 +63,7 @@ db.pedidos.aggregate([
 
 // TEXT/SEARCH: busca todos os lanches que custam menos de 5 reais e têm coxinha em seu nome
 db.lanches.createIndex({ nome: "text" });
-db.lanches.find({ $text: { $search: "coxinha" }, preco: { $lt: 5.00 }}, { nome: 1, preco: 1, tempo_preparo: 1, _id: 0 }).pretty();
+db.lanches.find({ $text: { $search: "coxinha" }, preco: { $lt: 5.00 }}, { nome: 1, preco: 1, tempo_preparo: 1, vegetariano: 1, _id: 0 }).pretty();
 
 // FILTER: busca todos os itens em todos os cardápios que tenham preço menor ou igual a 10 reais, com limite de 5 por cardápio
 db.lanchonetes.aggregate([
@@ -101,7 +101,7 @@ db.lanches.updateOne(
         $set: { tempo_preparo: 5 }
     }
 );
-db.lanches.find({ nome: "Pastel de Camarão" }, { nome: 1, preco: 1, tempo_preparo: 1, _id: 0 });
+db.lanches.find({ nome: "Pastel de Camarão" }, { nome: 1, preco: 1, tempo_preparo: 1, vegetariano: 1, _id: 0 });
 
 // FINDONE: busca a primeira lanchonete, em ordem de inserção, que tenha menos de 40 funcionários
 db.lanchonetes.findOne(
@@ -146,12 +146,12 @@ db.lanchonetes.aggregate([
 
 // SAVE (INSERTONE): insere um novo lanche com nome "Pastel Mistão", preço 17 reais e tempo de preparo 15 minutos, mas sem especificar o _id
 // save foi substituído pelo insertOne ou insertMany
-db.lanches.insertOne({ nome: "Pastel Mistão", preco: 17.00, tempo_preparo: 15 });
+db.lanches.insertOne({ nome: "Pastel Mistão", preco: 17.00, tempo_preparo: 15, vegetariano: false });
 
 // ADDTOSET: adiciona o novo lanche ao cardápio da lanchonete "Rango Top"
 db.lanchonetes.updateOne(
     { nome: "Rango Top" },
-    { $addToSet: { cardapio: { _id: ObjectId("64449fc089549bfcbe33690b"), nome: "Pastel Mistão", preco: 17.00, tempo_preparo: 15  } } }
+    { $addToSet: { cardapio: { _id: ObjectId("64449fc089549bfcbe33690b"), nome: "Pastel Mistão", preco: 17.00, tempo_preparo: 15, vegetariano: true  } } }
 );
 
 
